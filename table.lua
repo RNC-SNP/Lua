e140957a91ace28dc3bee1850f9c9444da28919d
RincLiu <i@RincLiu.com>
2014-12-02 02:19:31 -0800

    LinkedLIst.

--- table.lua
+++ table.lua
@@ -6,22 +6,40 @@ tab2 = tab1 -- `tab2` refers to the same table as `tab1`.
 tab2["x"] = tab2["x"] + 1 -- Lua doesn't support `++` and `+=` operator.
 print(tab1.x) -- A syntactic sugar: provide a.name for a["name"]. 
 
+
+
 --[[
 To represent a conventional Array, just use a table with integer keys. 
 There's no way to declare its size, just initialize the elements you need.
 --]]
-array = {}
+array1 = {"Android", "iOS", "WindowsPhone"}
+
+array2 = {}
 for i = 1, 10 do -- Indexes of an Array start with one (not with zero, as in C).
-	array[i] = i * i
+	array2[i] = i * i
 end
-for i, line in ipairs(array) do -- Iterate over the Array's elements.
+for i, line in ipairs(array2) do -- Iterate over the Array's elements.
 	print(i .. ": " .. line)
 end
 
+
+
 --[[
-When a program has no references to a table left, 
-Lua memory management will eventually delete the table and reuse its memory.
+Use table as record
 --]]
-tab1 = nil
-tab2 = nil
-array = nil
+point = {x = 0, y = 0, z = 0} -- It's equivalent to `point={}; point.x=0; point.y=0; point.z=0`
+
+
+
+--[[
+Use table to implement a linked-list
+--]]
+list = nil
+for line in io.lines() do
+	list = {value = line, next = list}
+end
+p = list
+while p do
+	print(p.value)
+	p = p.next
+end
