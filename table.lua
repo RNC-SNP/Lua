41746035f764836c930477694078467434363bcd
RincLiu <i@RincLiu.com>
2014-12-08 23:27:57 -0800

    Mix record-style and list-style table initializations.

--- table.lua
+++ table.lua
@@ -1,3 +1,7 @@
+--[[
+No matter what constructor we use to create a table, we can always add and remove other fields of any type to it.
+That is, all tables are created equal; constructors only affect their initialization.
+--]]
 tab1 = {} -- Create a table and store its reference in `tab1`.
 tab1["x"] = 8 -- The key's type and the value's are both dynamic.
 tab1[9] = "x"
@@ -29,6 +33,16 @@ Use table as record
 --]]
 point = {x = 0, y = 0, z = 0} -- It's equivalent to `point={}; point.x=0; point.y=0; point.z=0`
 
+
+
+--[[
+We can mix record-style and list-style initializations in the same constructor.
+--]]
+days = {[0] = "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
+print(0 .. ": " .. days[0])
+for i, line in ipairs(days) do
+	print(i .. ": " .. line)
+end
 
 
 --[[
